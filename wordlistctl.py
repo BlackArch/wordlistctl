22caaca63420df01126d50601c1a844166677c43
Sepehrdad Sh <sepehrdad.dev@gmail.com>
2020-04-16 18:54:27 +0200

    refactor names and add annotations

--- wordlistctl.py
+++ wordlistctl.py
@@ -12,94 +12,115 @@
 ################################################################################
 
 
+import argparse
+import gzip
+import tarfile
+import time
+import json
+import os
+import sys
+from shutil import copyfileobj
+from concurrent.futures import ThreadPoolExecutor
+
+try:
+    import requests
+    from termcolor import colored
+except Exception as ex:
+    print(f"[-] {ex}", file=sys.stderr)
+    sys.exit(-1)
+
+
 # Internal Variables
-__organization__ = "blackarch.org"
-__license__ = "GPLv3"
-__version__ = "v0.8.8-dev"
-__project__ = "wordlistctl"
-__description__ = "Fetch, install and search wordlist archives from websites."
+__organization__: str = "blackarch.org"
+__license__: str = "GPLv3"
+__version__: str = "v0.8.8-dev"
+__project__: str = "wordlistctl"
+__description__: str = "Fetch, install and search wordlist archives from websites."
 
 
-wordlist_path = "/usr/share/wordlists"
-repo = {}
-retry_count = 5
+WORDLIST_PATH: str = "/usr/share/wordlists"
+REPOSITORY: dict = {}
+RETRY_COUNT: int = 5
 
 
-def error(string):
+def error(string: str) -> None:
     print(colored("[-]", "red", attrs=["bold"]) +
           f" {string}", file=sys.stderr)
 
 
-def warning(string):
+def warning(string: str) -> None:
     print(colored("[!]", "yellow", attrs=["bold"]) + f" {string}")
 
 
-def info(string):
+def info(string: str) -> None:
     print(colored("[*]", "blue", attrs=["bold"]) + f" {string}")
 
 
-def success(string):
+def success(string: str) -> None:
     print(colored("[+]", "green", attrs=["bold"]) + f" {string}")
 
 
-def banner():
+def banner() -> None:
     print(colored(f"--==[ {__project__} by {__organization__} ]==--\n",
                   "red", attrs=["bold"]))
 
 
-def load_repo():
-    global repo
-    repofile = f"{os.path.dirname(os.path.realpath(__file__))}/repo.json"
+def load_repo() -> None:
+    global REPOSITORY
+    repofile: str = f"{os.path.dirname(os.path.realpath(__file__))}/repo.json"
     try:
         if not os.path.isfile(repofile):
             raise FileNotFoundError("repository file not found")
-        repo = json.load(open(repofile, 'r'))
+        REPOSITORY = json.load(open(repofile, 'r'))
     except Exception as ex:
         error(f"Error while loading repository: {str(ex)}")
         exit(-1)
 
 
-def to_readable_size(size):
-    units = {0: 'B',
-             1: 'Kb',
-             2: 'Mb',
-             3: 'Gb',
-             4: 'Tb'}
-    i = 0
+def to_readable_size(size: int) -> str:
+    units: dict = {0: 'B',
+                   1: 'Kb',
+                   2: 'Mb',
+                   3: 'Gb',
+                   4: 'Tb'}
+    i: int = 0
     while size > 1000:
         size = size / 1000
         i += 1
     return f"{size:.2f} {units[i]}"
 
 
-def decompress_file(infilename):
-    filename = os.path.basename(infilename).lower()
+def decompress_file(infilename: str) -> None:
+    filename: str = os.path.basename(infilename).lower()
     try:
+        info(f"decompressing {infilename}")
         if filename.endswith(".tar.gz"):
-            tar = tarfile.open(infilename)
+            tar: tarfile.TarFile = tarfile.open(infilename)
             tar.extractall(os.path.dirname(infilename))
         elif filename.endswith(".gz"):
-            gf = gzip.GzipFile(infilename)
+            gf: gzip.GzipFile = gzip.GzipFile(infilename)
             outfile = open(infilename.split(".gz")[0], "wb")
             copyfileobj(gf, outfile)
             outfile.close()
         else:
+            warning(f"decompressing {infilename.split('.')[-1]} file type not supported")
             return
+        success(f"decompressing {filename} completed")
         os.remove(infilename)
     except Exception as ex:
-        error(f"Unable to decompress file: {ex}")
+        error(f"Unable to decompress {infilename}: {ex}")
 
 
-def fetch_file(url, path, useragent, decompress):
-    filename = os.path.basename(path)
+def fetch_file(url: str, path: str, useragent: str, decompress: bool) -> None:
+    filename: str = os.path.basename(path)
     try:
         if os.path.isfile(path):
             warning(f"{filename} already exists -- skipping")
         else:
             info(f"downloading {filename} to {path}")
-            for retry in range(retry_count):
-                rq = requests.get(url, stream=True,
-                                headers={"User-Agent": useragent})
+            for retry in range(RETRY_COUNT):
+                rq: requests.Response = requests.get(url, stream=True,
+                                                     headers={"User-Agent": useragent})
                 if rq.status_code == 404:
                     raise FileNotFoundError("host returned 404")
                 elif rq.status_code != 200:
@@ -119,7 +140,7 @@ def fetch_file(url, path, useragent, decompress):
         error(f"Error while downloading {filename}: {ex}")
 
 
-def check_dir(dir_name):
+def check_dir(dir_name: str) -> None:
     try:
         if os.path.isdir(dir_name) is False:
             info(f"creating directory {dir_name}")
@@ -129,12 +150,17 @@ def check_dir(dir_name):
         exit(-1)
 
 
-def fetch_func(parser):
+def fetch_func(parser: argparse.ArgumentParser) -> None:
+    global REPOSITORY
+
     if parser.wordlist is None and parser.group is None:
         error("no wordlist specified")
         return
 
-    executer = ThreadPoolExecutor(parser.workers)
+    if parser.workers > 25:
+        warning("Number of workers is too big, you might get banned.")
+
+    executer: ThreadPoolExecutor = ThreadPoolExecutor(parser.workers)
 
     check_dir(parser.basedir)
 
@@ -142,32 +168,34 @@ def fetch_func(parser):
                   "discovery", "fuzzing", "misc"]:
         check_dir(f"{parser.basedir}/{group}")
 
-    wordlists = []
+    wordlists: list = []
 
     if parser.wordlist is not None:
         wordlists = [wordlist for wordlist in parser.wordlist]
 
     if parser.group is not None:
-        for wordlist in repo:
-            if repo[wordlist]["group"] in parser.group:
+        for wordlist in REPOSITORY:
+            if REPOSITORY[wordlist]["group"] in parser.group:
                 wordlists.append(wordlist)
 
     for wordlist in wordlists:
-        if wordlist not in repo:
+        if wordlist not in REPOSITORY:
             error(f"wordlist not found: {wordlist}")
             continue
-        group = repo[wordlist]["group"]
-        filename = repo[wordlist]["url"].split('/')[-1]
-        path = f"{parser.basedir}/{group}/{filename}"
-        executer.submit(fetch_file, repo[wordlist]["url"], path,
+        group: str = REPOSITORY[wordlist]["group"]
+        filename: str = REPOSITORY[wordlist]["url"].split('/')[-1]
+        path: str = f"{parser.basedir}/{group}/{filename}"
+        executer.submit(fetch_file, REPOSITORY[wordlist]["url"], path,
                         parser.useragent, parser.decompress)
-
 
     executer.shutdown(wait=True)
 
-def search_func(parser):
-    count = 0
-    search_term = parser.search_term
+
+def search_func(parser: argparse.ArgumentParser) -> None:
+    global REPOSITORY
+
+    count: int = 0
+    search_term: str = parser.search_term
     try:
         if parser.local:
             for root, _, files in os.walk(parser.basedir):
@@ -178,9 +206,9 @@ def search_func(parser):
                         print(f"    > {wordlist} ({size})")
                         count += 1
         else:
-            for wordlist in repo:
+            for wordlist in REPOSITORY:
                 if wordlist.__contains__(search_term):
-                    size = repo[wordlist]["size"]
+                    size = REPOSITORY[wordlist]["size"]
                     print(f"    > {wordlist} ({size})")
                     count += 1
 
@@ -190,22 +218,24 @@ def search_func(parser):
         error(ex)
 
 
-def lst_func(parser):
+def lst_func(parser: argparse.ArgumentParser) -> None:
+    global REPOSITORY
+
     success("available wordlists:")
 
     print()
 
-    for wordlist in repo:
+    for wordlist in REPOSITORY:
         if parser.group is not None:
-            if repo[wordlist]["group"] not in parser.group:
+            if REPOSITORY[wordlist]["group"] not in parser.group:
                 continue
-        size = repo[wordlist]["size"]
+        size: str = REPOSITORY[wordlist]["size"]
         print(f"    > {wordlist} ({size})")
 
     print()
 
 
-def main():
+def main() -> int:
     banner()
 
     load_repo()
@@ -230,7 +260,7 @@ def main():
                        help="download workers [default: %(default)s]")
     fetch.add_argument("-u", "--useragent", default=f"{__project__}/{__version__}",
                        help="fetch user agent [default: %(default)s]")
-    fetch.add_argument("-b", "--base-dir", default=f"{wordlist_path}", dest="basedir",
+    fetch.add_argument("-b", "--base-dir", default=f"{WORDLIST_PATH}", dest="basedir",
                        help="wordlists base directory [default: %(default)s]")
 
     fetch.set_defaults(func=fetch_func)
@@ -239,7 +269,7 @@ def main():
     search.add_argument("search_term", help="what to search")
     search.add_argument("-l", "--local", action="store_true", default=False,
                         help="search local archives")
-    search.add_argument("-b", "--base-dir", default=f"{wordlist_path}", dest="basedir",
+    search.add_argument("-b", "--base-dir", default=f"{WORDLIST_PATH}", dest="basedir",
                         help="wordlists base directory [default: %(default)s]")
     search.set_defaults(func=search_func)
 
@@ -263,20 +293,4 @@ def main():
 
 
 if __name__ == "__main__":
-    import argparse
-    import gzip
-    import tarfile
-    import time
-    import json
-    import os
-    import sys
-    from shutil import copyfileobj
-    from concurrent.futures import ThreadPoolExecutor
-
-    try:
-        import requests
-        from termcolor import colored
-    except Exception as ex:
-        print(f"[-] {ex}", file=sys.stderr)
-        sys.exit(-1)
     sys.exit(main())
